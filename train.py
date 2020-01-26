36e85a4a07fd1b7565bab577e2ff185a6b57b8fe
guigui10011 <guigui.cavaro@aliceadsl.fr>
2020-01-26 19:07:39 +0100

    ...

--- train.py
+++ train.py
@@ -50,8 +50,6 @@ if __name__ == '__main__':
     model.setup(opt)               # regular setup: load and print networks; create schedulers
     total_iters = 0                # the total number of training iterations
 
-    model.save_networks('latest')
-
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
