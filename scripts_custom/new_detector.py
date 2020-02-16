de43cbd8228d6681db6603bb7493001ba37cdc13
guigui10011 <guigui.cavaro@aliceadsl.fr>
2020-02-16 13:05:18 +0100

    aa

--- scripts_custom/new_detector.py
+++ scripts_custom/new_detector.py
@@ -86,12 +86,12 @@ rects = detector(gray, 1)
 
 
 # loop over the face detections
-print(len(rects))
 for (i, rect) in enumerate(rects):
 	# determine the facial landmarks for the face region, then
 	# convert the landmark (x, y)-coordinates to a NumPy array
 	shape = predictor(gray, rect)
 	shape = face_utils.shape_to_np(shape)
+	print(shape)
 	# loop over the face parts individually
 	for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
 		# clone the original image so we can draw on it, then
