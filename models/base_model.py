707ba77a652a6f326d9e340c7110d9ae93f5c700
guigui10011 <guigui.cavaro@aliceadsl.fr>
2020-07-07 22:22:27 +0200

    added strict=false to load_state_dict

--- models/base_model.py
+++ models/base_model.py
@@ -221,7 +221,7 @@ class BaseModel(ABC):
                     state_dict[missings[i]] = state_dict.pop(unexpecteds[i])
                 # End fix
 
-                net.load_state_dict(state_dict)
+                net.load_state_dict(state_dict, strict=False)
 
     def print_networks(self, verbose):
         """Print the total number of parameters in the network and (if verbose) network architecture
